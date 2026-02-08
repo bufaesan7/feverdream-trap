99be535b6608ce58e97e81f2acf3935324ad4a75
KirmesBude <kirmesbude@gmail.com>
2026-02-08 13:28:17 +0100

    Cursor grab

--- game/src/menus/pause.rs
+++ game/src/menus/pause.rs
@@ -2,13 +2,22 @@
 
 use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
-use crate::{menus::Menu, screens::Screen, theme::widget};
+use crate::{
+    camera_controller::{cursor_grab, cursor_ungrab},
+    menus::Menu,
+    screens::Screen,
+    theme::widget,
+};
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(OnEnter(Menu::Pause), spawn_pause_menu);
+    app.add_systems(OnEnter(Menu::Pause), (spawn_pause_menu, cursor_ungrab));
     app.add_systems(
         Update,
         go_back.run_if(in_state(Menu::Pause).and(input_just_pressed(KeyCode::Escape))),
+    );
+    app.add_systems(
+        OnExit(Menu::Pause),
+        cursor_grab.run_if(in_state(Screen::Gameplay)),
     );
 }
 
