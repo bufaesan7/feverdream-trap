094d21e31ff1fbc7b36a51ce0ad204fc79eb5c7e
KirmesBude <kirmesbude@gmail.com>
2026-02-08 10:42:26 +0100

    simplify asset plugin initialization

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,22 +3,18 @@ use bevy::{asset::AssetMetaCheck, prelude::*};
 pub mod prelude;
 
 pub fn asset_plugin() -> AssetPlugin {
+    let mut asset_plugin = AssetPlugin {
+        meta_check: AssetMetaCheck::Never,
+        ..Default::default()
+    };
+
     #[cfg(all(feature = "dev", not(target_arch = "wasm32")))]
     {
         let package = env!("CARGO_PKG_NAME").split("_").last().unwrap();
         let asset_path = env!("CARGO_MANIFEST_DIR").replacen(package, "assets", 1);
 
-        AssetPlugin {
-            file_path: asset_path,
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }
+        asset_plugin.file_path = asset_path;
     }
-    #[cfg(any(not(feature = "dev"), target_arch = "wasm32"))]
-    {
-        AssetPlugin {
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }
-    }
+
+    asset_plugin
 }
