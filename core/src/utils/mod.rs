391ff0b7feb09424bb2db280ae7cca256668d8ac
KirmesBude <kirmesbude@gmail.com>
2026-02-14 15:34:16 +0100

    DespawnAfter helper

--- core/src/utils/mod.rs
+++ core/src/utils/mod.rs
@@ -1,2 +1,29 @@
 pub mod audio;
 pub mod cursor;
+
+use crate::prelude::*;
+
+#[derive(Debug, Component, Reflect)]
+#[reflect(Component)]
+pub struct DespawnAfter(Timer);
+
+impl DespawnAfter {
+    pub fn new(duration: Duration) -> Self {
+        Self(Timer::new(duration, TimerMode::Once))
+    }
+
+    pub fn from_secs_f32(secs: f32) -> Self {
+        Self::new(Duration::from_secs_f32(secs))
+    }
+}
+
+pub fn despawn_after_internal(
+    commands: &mut Commands,
+    entity: Entity,
+    despawn_after: &mut DespawnAfter,
+    delta: Duration,
+) {
+    if despawn_after.0.tick(delta).just_finished() {
+        commands.entity(entity).try_despawn();
+    }
+}
--- game/src/utils/mod.rs
+++ game/src/utils/mod.rs
@@ -1,1 +1,21 @@
 pub mod audio;
+
+use crate::prelude::*;
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_systems(
+        Update,
+        despawn_after.run_if(in_state(Screen::Gameplay).and(in_state(Menu::None))),
+    );
+}
+
+// Schedule this so it only runs when paused
+pub fn despawn_after(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut DespawnAfter)>,
+) {
+    for (entity, mut despawn_after) in &mut query {
+        despawn_after_internal(&mut commands, entity, &mut despawn_after, time.delta());
+    }
+}
