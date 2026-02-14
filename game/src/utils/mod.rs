7f7032eb5a6ed6bc55d947f7bb8f6594a1b9d718
KirmesBude <kirmesbude@gmail.com>
2026-02-14 17:38:37 +0100

    "Fix" the scheduling

--- game/src/utils/mod.rs
+++ game/src/utils/mod.rs
@@ -3,10 +3,12 @@ pub mod audio;
 use crate::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins(audio::plugin)
-        .add_systems(
-            Update,
-            (despawn_after, fade_text).run_if(in_state(Screen::Gameplay).and(in_state(Menu::None))),
-        )
-        .add_systems(Update, fade_sound); // A bit weird, but we want to fade out the music from the gameplay on the title screen, but also means it will not be despawned until later since that only runs in Gameplay
+    app.add_plugins(audio::plugin).add_systems(
+        Update,
+        (despawn_after, fade_text, fade_sound).run_if(
+            in_state(Screen::Gameplay)
+                .and(in_state(Menu::None))
+                .or(not(in_state(Screen::Gameplay))),
+        ),
+    );
 }
